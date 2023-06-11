921de0a5f15246536ec4adb5050d149133a25db3
Ema Gomez <emagozz@gmail.com>
2023-06-11 20:10:35 -0300

    Minor changes to deploy PROD

---
+++ firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyAcORVnQWeBmXlbm9lCWWR3rLLlmlTTqV8',
+  authDomain: 'wandererhub-cf345.firebaseapp.com',
+  projectId: 'wandererhub-cf345',
+  storageBucket: 'wandererhub-cf345.appspot.com',
+  messagingSenderId: '541553080559',
+  appId: '1:541553080559:web:a3684e459f0885f0cd431a',
+  measurementId: 'G-XPHPTJHNGJ',
+};
+
+export default firebaseConfig;
