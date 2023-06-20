0f5fb1aca53eba2e8361d8eb6f0dff5a02040f87
Ema Gomez <emagozz@gmail.com>
2023-06-20 20:20:42 -0300

    Fix problems with keys

--- firebase.ts
+++ firebase.ts
@@ -2,16 +2,16 @@ import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 import dotenv from 'dotenv'
 
-dotenv.config() // Cargar variables de entorno desde el archivo .env
+const FIREBASE_CONFIG = dotenv.config().parsed
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+  apiKey: `${FIREBASE_CONFIG?.FIREBASE_API_KEY}`,
+  authDomain: `${FIREBASE_CONFIG?.FIREBASE_AUTH_DOMAIN}`,
+  projectId: `${FIREBASE_CONFIG?.FIREBASE_PROJECT_ID}`,
+  storageBucket: `${FIREBASE_CONFIG?.FIREBASE_STORAGE_BUCKET}`,
+  messagingSenderId: `${FIREBASE_CONFIG?.FIREBASE_MESSAGING_SENDER_ID}`,
+  appId: `${FIREBASE_CONFIG?.FIREBASE_APP_ID}`,
+  measurementId: `${FIREBASE_CONFIG?.FIREBASE_MEASUREMENT_ID}`,
 }
 
 let dbInstance: ReturnType<typeof getFirestore> | null = null
