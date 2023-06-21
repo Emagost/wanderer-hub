b3784bc2d7d1624d20d01b2c9e3e7edf9e44b482
Ema Gomez <emagozz@gmail.com>
2023-06-20 22:15:23 -0300

    Minor fixes

--- firebase.ts
+++ firebase.ts
@@ -1,17 +1,16 @@
 import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
-import dotenv from 'dotenv'
 
-const FIREBASE_CONFIG = dotenv.config().parsed
+console.log('process.env', process.env.API_KEY)
 
 const firebaseConfig = {
-  apiKey: `${FIREBASE_CONFIG?.FIREBASE_API_KEY}`,
-  authDomain: `${FIREBASE_CONFIG?.FIREBASE_AUTH_DOMAIN}`,
-  projectId: `${FIREBASE_CONFIG?.FIREBASE_PROJECT_ID}`,
-  storageBucket: `${FIREBASE_CONFIG?.FIREBASE_STORAGE_BUCKET}`,
-  messagingSenderId: `${FIREBASE_CONFIG?.FIREBASE_MESSAGING_SENDER_ID}`,
-  appId: `${FIREBASE_CONFIG?.FIREBASE_APP_ID}`,
-  measurementId: `${FIREBASE_CONFIG?.FIREBASE_MEASUREMENT_ID}`,
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID,
 }
 
 let dbInstance: ReturnType<typeof getFirestore> | null = null
--- next.config.js
+++ next.config.js
@@ -5,15 +5,25 @@ const securityHeaders = [
     key: 'Cross-Origin-Opener-Policy',
     value: 'same-origin',
   },
-];
+]
 const nextConfig = {
+  reactStrictMode: true,
+  env: {
+    API_KEY: process.env.API_KEY,
+    AUTH_DOMAIN: process.env.AUTH_DOMAIN,
+    PROJECT_ID: process.env.PROJECT_ID,
+    STORAGE_BUCKET: process.env.STORAGE_BUCKET,
+    MESSAGING_SENDER_ID: process.env.MESSAGING_SENDER_ID,
+    APP_ID: process.env.APP_ID,
+    MEASUREMENT_ID: process.env.MEASUREMENT_ID,
+  },
   async headers() {
     return [
       {
         source: '/(.*)',
         headers: securityHeaders,
       },
-    ];
+    ]
   },
   images: {
     remotePatterns: [
@@ -23,6 +33,6 @@ const nextConfig = {
       },
     ],
   },
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
