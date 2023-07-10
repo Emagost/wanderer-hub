03aaad96eed8814c42ff897b328b21398081bcb3
Ema Gomez <emagozz@gmail.com>
2023-07-09 22:27:31 -0300

    Continue with profile

--- src/app/userProfile/[uid]/page.tsx
+++ src/app/userProfile/[uid]/page.tsx
@@ -11,7 +11,7 @@ import EditIcon from '@/app/Components/icons/EditIcon'
 const UserProfile = ({ params }: { params: { uid: string } }) => {
   const [isEditing, setIsEditing] = useState<boolean>(false)
 
-  const { db, user } = useAuth()
+  const { db, user, logout } = useAuth()
 
   const userQuery = useMemo(
     () => query(collection(db, 'users'), where('uid', '==', params.uid)),
@@ -40,30 +40,11 @@ const UserProfile = ({ params }: { params: { uid: string } }) => {
     setUserName(event.target.value)
   }
 
-  //   <button
-  //     className="bg-buttonPrimary hover:bg-buttonPrimaryHover font-bold py-2 px-4 rounded-md"
-  //     onClick={logout}
-  //   >
-  //     Logout
-  //   </button>
-  // </div>
-  // <div className="absolute top-4 left-4">
-  //   <Link href="/generalChat">
-  //     <button className="bg-buttonPrimary hover:bg-buttonPrimaryHover font-bold py-2 px-4 rounded-md">
-  //       Go back
-  //     </button>
-  //   </Link>
-  // </div>
-  //         <Image
-  //           className="mb-3 rounded-full z-10"
-
-  //         />
-
   return (
     <>
       {status === 'success' && user && (
-        <main className="profile-page">
-          <section className="relative block h-96">
+        <main className="h-screen w-screen">
+          <section className="relative block h-1/3">
             <div
               className="absolute top-0 w-full h-full bg-center bg-cover"
               style={{
@@ -73,27 +54,23 @@ const UserProfile = ({ params }: { params: { uid: string } }) => {
             >
               <span id="blackOverlay" className="w-full h-full absolute opacity-50 bg-black"></span>
             </div>
-            <div
-              className="top-auto bottom-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden h-16"
-              style={{ transform: 'translateZ(0px)' }}
-            >
-              <svg
-                className="absolute bottom-0 overflow-hidden"
-                xmlns="http://www.w3.org/2000/svg"
-                preserveAspectRatio="none"
-                version="1.1"
-                viewBox="0 0 2560 100"
-                x="0"
-                y="0"
+            <div className="relative z-10">
+              <button
+                className="bg-pink-500 hover:bg-pink-600 absolute top-5 right-5 font-bold py-2 px-4 rounded-md"
+                onClick={logout}
               >
-                <polygon
-                  className="text-gray-200 fill-current"
-                  points="2560 0 2560 100 0 100"
-                ></polygon>
-              </svg>
+                Logout
+              </button>
+            </div>
+            <div className="relative z-10">
+              <Link href="/generalChat">
+                <button className="bg-pink-500 hover:bg-pink-600 absolute top-5 left-5 font-bold py-2 px-4 rounded-md">
+                  Go back
+                </button>
+              </Link>
             </div>
           </section>
-          <section className="relative py-16 bg-blue-500-200">
+          <section className="relative py-16 bg-gray-200 h-4/6 max-h-4/6">
             <div className="container mx-auto px-4">
               <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-xl rounded-lg -mt-64">
                 <div className="px-6">
@@ -112,10 +89,13 @@ const UserProfile = ({ params }: { params: { uid: string } }) => {
                     <div className="w-full lg:w-4/12 px-4 lg:order-3 lg:text-right lg:self-center">
                       <div className="py-6 px-3 mt-32 sm:mt-0">
                         <button
-                          className="bg-pink-500 active:bg-pink-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150"
+                          className="bg-pink-500 hover:bg-pink-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150"
                           type="button"
                         >
                           Connect
+                        </button>
+                        <button onClick={handleEdit} className="color-black bg-black">
+                          <EditIcon />
                         </button>
                       </div>
                     </div>
@@ -131,42 +111,44 @@ const UserProfile = ({ params }: { params: { uid: string } }) => {
                           <span className="text-xl font-bold block uppercase tracking-wide text-blue-600">
                             10
                           </span>
-                          <span className="text-sm text-gray-400">Photos</span>
-                        </div>
-                        <div className="lg:mr-4 p-3 text-center">
-                          <span className="text-xl font-bold block uppercase tracking-wide text-blue-600">
-                            89
-                          </span>
-                          <span className="text-sm text-gray-400">Comments</span>
+                          <span className="text-sm text-gray-400">Travels</span>
                         </div>
                       </div>
                     </div>
                   </div>
-                  <div className="text-center mt-12">
-                    <h3 className="text-4xl font-semibold leading-normal mb-2 text-gray-700">
-                      Jenna Stones
-                    </h3>
+                  <div className="text-center">
+                    {isEditing ? (
+                      <input
+                        type="text"
+                        onChange={handleEditUsername}
+                        value={userName}
+                        className="text-4xl font-semibold leading-normal mb-2 text-gray-700"
+                      />
+                    ) : (
+                      <h3 className="text-4xl font-semibold leading-normal mb-2 text-gray-700">
+                        {userName}
+                      </h3>
+                    )}
                     <div className="text-sm leading-normal mt-0 mb-2 text-gray-400 font-bold uppercase">
                       <i className="mr-2 text-lg text-gray-400"></i>
                       Los Angeles, California
-                    </div>
-                    <div className="mb-2 text-gray-600 mt-10">
-                      <i className="mr-2 text-lg text-gray-400"></i>Solution Manager - Creative Tim
-                      Officer
-                    </div>
-                    <div className="mb-2 text-gray-600">
-                      <i className="mr-2 text-lg text-gray-400"></i>University of Computer Science
                     </div>
                   </div>
                   <div className="mt-10 py-10 border-t border-gray-200 text-center">
                     <div className="flex flex-wrap justify-center">
                       <div className="w-full lg:w-9/12 px-4">
-                        <p className="mb-4 text-lg leading-relaxed text-gray-700">
-                          An artist of considerable range, Jenna the name taken by Melbourne-raised,
-                          Brooklyn-based Nick Murphy writes, performs and records all of his own
-                          music, giving it a warm, intimate feel with a solid groove structure. An
-                          artist of considerable range.
-                        </p>
+                        {isEditing ? (
+                          <input
+                            type="text"
+                            onChange={handleEditDescription}
+                            value={description}
+                            className="mb-4 text-lg leading-relaxed text-gray-700"
+                          />
+                        ) : (
+                          <p className="mb-4 text-lg leading-relaxed text-gray-700">
+                            {description}
+                          </p>
+                        )}
                       </div>
                     </div>
                   </div>
